8c0c6158b889f9f51850519085a1e86e4e8daacf
Liu-ming147 <1479763099@qq.com>
2021-10-29 22:02:48 +0800

    oj练习

--- DataStructure/src/oj/GetValueK.java
+++ DataStructure/src/oj/GetValueK.java
@@ -1,20 +1,42 @@
 package oj;
 
+/**
+ * 链表中倒数第k个结点(牛客)
+ */
 public class GetValueK {
-    // 链表中倒数第k个结点(牛客)
+//    // 方法1
+//    public ListNode FindKthToTail(ListNode head,int k) {
+//        int length = 0;
+//        ListNode listNode = head;
+//        while (listNode != null) {
+//            listNode = listNode.next;
+//            length++;
+//        }
+//        if (length < k) {
+//            return null;
+//        }
+//        for (int i = 0; i < length - k; i++) {
+//            head = head.next;
+//        }
+//        return head;
+//    }
+    // 方法2：快慢指针
     public ListNode FindKthToTail(ListNode head,int k) {
-        int length = 0;
-        ListNode listNode = head;
-        while (listNode != null) {
-            listNode = listNode.next;
-            length++;
-        }
-        if (length < k) {
+        if (head == null || k == 0) {
             return null;
         }
-        for (int i = 0; i < length - k; i++) {
-            head = head.next;
+        ListNode low = head;
+        ListNode fast = head;
+        for (int i = 0; i < k; i++) {
+            if (fast == null) {
+                return null;
+            }
+            fast = fast.next;
         }
-        return head;
+        while (fast != null) {
+            low = low.next;
+            fast = fast.next;
+        }
+        return low;
     }
 }
